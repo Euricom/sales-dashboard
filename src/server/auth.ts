79f7fd45d6262600c48a4757a27a6c92c0c3dc11
Stefano P <stefano.peeters@gmail.com>
2024-02-25 23:31:42 +0100

    [init] initial commit

---
+++ src/server/auth.ts
@@ -0,0 +1,349 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+  type Session,
+} from "next-auth";
+import { type Adapter } from "next-auth/adapters";
+import AzureProvider from "next-auth/providers/azure-ad";
+import type { JWT } from "next-auth/jwt";
+import {env} from "~/env";
+import {db} from "~/server/db";
+
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next" {
+  /**
+   * Used with withAuth helper is applied to a NextApiHandler
+   */
+  interface NextApiRequest {
+    nextauth: {
+      token: JWT;
+      session: Session;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  /**
+   * Returned by the `jwt` callback and `getToken`, when using JWT sessions
+   */
+  interface JWT {
+    // default
+    name?: string | null;
+    email?: string | null;
+    picture?: string | null;
+    sub?: string;
+
+    // extended
+    accessToken: string;
+    refreshToken?: string;
+    expiresAt: number;
+    roles: string[];
+    oid: string;
+    iat: number;
+    exp: number;
+    jti: string;
+  }
+}
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop
+   * on the `SessionProvider` React Context
+   */
+  interface Session extends DefaultSession {
+    tokenExpiresAt: string;
+    tokenExpiresIn: string;
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      roles: string[];
+    };
+  }
+
+  /** Azure AD Account */
+  interface Account {
+    provider: string;
+    providerAccountId: string;
+    token_type: "Bearer";
+    scope: string;
+    expires_at: number;
+    ext_expires_in: number;
+    access_token: string;
+    id_token: string;
+    session_state: string;
+  }
+
+  /** Azure AD Profile (id_token payload) */
+  interface Profile {
+    aud: string;
+    iss: string; // https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/v2.0
+    iat: number;
+    nbf: number;
+    exp: number;
+    aio: string;
+    oid: string;
+    preferred_username: string;
+    rh: string;
+    roles: string[];
+    tid: string;
+    uti: string;
+    ver: "2.0";
+  }
+}
+
+type RefreshTokenPayload =
+  | {
+      token_type: "Bearer";
+      scope: string;
+      expires_in: number;
+      ext_expires_in: number;
+      refresh_token: string;
+      access_token: string;
+      id_token: string;
+    }
+  | {
+      error: string;
+      error_description: string;
+      error_codes: number[];
+      error_uri: string;
+    };
+
+/*
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `expiresAt`.
+ */
+const refreshAccessToken = async (token: JWT): Promise<JWT> => {
+  try {
+    if (!token.refreshToken) {
+      throw new Error("No refresh token available.");
+    }
+
+    const response = await fetch(
+      `https://login.microsoftonline.com/${env.AZURE_AD_TENANT_ID}/oauth2/v2.0/token`,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+          grant_type: "refresh_token",
+          client_id: env.AZURE_AD_CLIENT_ID ?? "",
+          client_secret: env.AZURE_AD_CLIENT_SECRET ?? "",
+          refresh_token: token.refreshToken,
+          scope: "openid profile email offline_access",
+        }),
+        method: "POST",
+      },
+    );
+    const data: RefreshTokenPayload =
+      (await response.json()) as RefreshTokenPayload;
+    if ("error" in data) {
+      throw new Error(`Failed to refresh accessToken: ${data.error}`);
+    }
+    if (!response.ok) {
+      throw new Error(`Request failed: ${response.status}`);
+    }
+    return {
+      ...token,
+      accessToken: data.access_token,
+      expiresAt: Date.now() + data.expires_in * 1000,
+      refreshToken: data.refresh_token,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Failed to refresh token:", error.message);
+    }
+
+    // throwing the error will cause the session to be invalidated
+    // and the user must re-login
+    throw error;
+  }
+};
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authOptions: NextAuthOptions = {
+  // callbacks: {
+  //   session: ({ session, user }) => ({
+  //     ...session,
+  //     user: {
+  //       ...session.user,
+  //       id: user.id,
+  //     },
+  //   }),
+  // },
+  session: {
+    maxAge: 1 * 24 * 60 * 60, // 24h - should be the same as the refresh token lifetime
+  },
+  // adapter: PrismaAdapter(db),
+  providers: [
+    AzureProvider({
+      clientId: env.AZURE_AD_CLIENT_ID ?? "",
+      clientSecret: env.AZURE_AD_CLIENT_SECRET ?? "",
+      tenantId: env.AZURE_AD_TENANT_ID ?? "",
+      // idToken: true,
+      checks: ["pkce"], // to prevent CSRF and authorization code injection attacks.
+      authorization: {
+        params: {
+          scope: "openid profile email offline_access",
+        },
+      },
+    }),
+    /**
+     * ...add more providers here.
+     *
+     * Most other providers require a bit more work than the Discord provider. For example, the
+     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+     *
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
+  callbacks: {
+    jwt({ token, account, user, profile }) {
+      // log.debug('jwt: %o', { token });
+      // Initial sign in
+      if (profile && account) {
+        delete user?.image; // lets keep auth cookie small
+        const clockSkew = 60 * 10 * 1000; // 10 minutes
+        const expiresAt = account.expires_at * 1000 - clockSkew; // ms
+        return {
+          expiresAt,
+          oid: profile.oid,
+          iat: token.iat,
+          exp: token.exp,
+          jti: token.jti,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          roles: profile.roles,
+          ...user,
+        };
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.expiresAt) {
+        return token;
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token);
+    },
+    session({ session, token }) {
+      if (session.user) {
+        // get time span until token expires
+        const date = new Date(0);
+        date.setMilliseconds(token.expiresAt - Date.now());
+        const timeSpanUntilExpires = date.toISOString().substring(11, 19);
+        const roles = token.roles || [];
+
+        // create session object
+        session.user.id = token.oid;
+        session.user.name = token.name ?? "";
+        session.user.email = token.email ?? "";
+        session.user.roles = roles;
+        session.tokenExpiresAt = new Date(token.expiresAt).toISOString();
+        session.tokenExpiresIn = timeSpanUntilExpires;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(db) as Adapter,
+};
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
+
+export type Role = "admin" | "user";
+/**
+ * Usages:
+ * ```
+ * const isAdmin = isInRole(user, 'admin');
+ * const isAllowed = isInRole(user, ['admin', 'user']);
+ * ```
+ *
+ * @returns true when the user has the given role or one of the roles
+ */
+export const isInRole = (
+  user: Session["user"] | undefined,
+  role: Role | Role[],
+) => {
+  const userRoles = user?.roles ?? [];
+  if (Array.isArray(role)) {
+    return role.some((r) => userRoles.includes(r));
+  }
+  return userRoles.includes(role);
+};
+
+/**
+ * @returns true when the user is authenticated
+ */
+export const isAuthenticated = (session: Session) => !!session;
+
+/**
+ * Get a user from the session or return the anonymous user.
+ */
+export const getServerUser = async () => {
+  const session = await getServerSession(authOptions);
+  return getUser(session);
+};
+
+/**
+ * User class with some helper methods to easy check roles and authentication.
+ */
+export class User {
+  id: string;
+  name: string;
+  email: string;
+  roles: string[];
+
+  constructor(data: Session["user"]) {
+    this.id = data.id;
+    this.name = data.name;
+    this.email = data.email;
+    this.roles = data.roles;
+  }
+
+  isInRole(role: Role | Role[]) {
+    return isInRole(this, role);
+  }
+
+  get isAuthenticated() {
+    return !!this.id;
+  }
+}
+
+/**
+ * Get a user from the session or return the anonymous user.
+ * Simplifies the usage of the user object (never null)
+ * @param session
+ */
+export const getUser = (session: Session | null) => {
+  return new User(
+    session?.user ?? {
+      // anonymous user
+      id: "",
+      name: "anonymous",
+      email: "",
+      roles: [],
+    },
+  );
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,132 @@
+import { type Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
+
+export default {
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "'./src/**/*.{js,ts,jsx,tsx}'",
+  ],
+  theme: {
+    colors: {
+      inherit: "inherit",
+      current: "current",
+      transparent: "transparent",
+      black: "#000000",
+      white: "#ffffff",
+      accent: {
+        50: "#e7ffe4",
+        100: "#c9ffc4",
+        200: "#98ff90",
+        300: "#56ff50",
+        400: "#00ff00",
+        500: "#00e606",
+        600: "#00b809",
+        700: "#008b07",
+        800: "#076d0d",
+        900: "#0b5c11",
+        DEFAULT: "#00ff00",
+      },
+      info: {
+        50: "#f0f9ff",
+        100: "#e0f2fe",
+        200: "#bae6fd",
+        300: "#7ed4fb",
+        400: "#3abef7",
+        500: "#0fa6e8",
+        600: "#0385c6",
+        700: "#046aa0",
+        800: "#085a84",
+        900: "#0d4b6d",
+        DEFAULT: "#3abef7",
+      },
+    },
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [
+    nextui({
+      layout: {
+        radius: {
+          small: "2px", // rounded-small
+          medium: "2px", // rounded-medium
+          large: "2px", // rounded-large
+        },
+      },
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              50: "#f0fafb",
+              100: "#d8eff5",
+              200: "#b7e0ea",
+              300: "#84c8dc",
+              400: "#52abc7",
+              500: "#308caa",
+              600: "#2a7190",
+              700: "#285d76",
+              800: "#284e62",
+              900: "#264253",
+              DEFAULT: "#52abc7",
+            },
+            secondary: {
+              50: "#f4f6f7",
+              100: "#e2e8eb",
+              200: "#c8d4d9",
+              300: "#a2b6be",
+              400: "#758f9b",
+              500: "#648290",
+              600: "#4d616d",
+              700: "#43525b",
+              800: "#3c474e",
+              900: "#353d44",
+              DEFAULT: "#648290",
+            },
+            success: {
+              50: "#ecfdf5",
+              100: "#d1fae5",
+              200: "#a7f3d0",
+              300: "#6ee7b7",
+              400: "#37d39a",
+              500: "#11b881",
+              600: "#059669",
+              700: "#047857",
+              800: "#065f46",
+              900: "#064e3b",
+              DEFAULT: "#37d39a",
+            },
+            warning: {
+              50: "#fffbeb",
+              100: "#fef3c7",
+              200: "#fde68a",
+              300: "#fbd24e",
+              400: "#fabd22",
+              500: "#f49d0c",
+              600: "#d87607",
+              700: "#b3520a",
+              800: "#91400f",
+              900: "#78350f",
+              DEFAULT: "#fabd22",
+            },
+            danger: {
+              50: "#fdf3f3",
+              100: "#fce7e8",
+              200: "#f9d2d7",
+              300: "#f3aeb7",
+              400: "#ec8091",
+              500: "#e4677e",
+              600: "#cc3256",
+              700: "#ab2548",
+              800: "#902141",
+              900: "#7b203d",
+              DEFAULT: "#e4677e",
+            },
+          },
+        },
+      },
+    }),
+  ],
+} satisfies Config;
