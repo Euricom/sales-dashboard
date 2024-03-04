3908fd38b1af5bbca4dc379482b96514748bd398
Stefano P <stefano.peeters@gmail.com>
2024-03-04 11:05:01 +0100

    [feat] added sharepoint correct flow

--- src/server/api/routers/sharepoint/index.ts
+++ src/server/api/routers/sharepoint/index.ts
@@ -2,7 +2,7 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { getEmployeesData, getToken } from "./utils";
 
 export const sharePointRouter = createTRPCRouter({
-  getEmployeesData: protectedProcedure.query(async () => {
+  getEmployeesData: publicProcedure.query(async () => {
     const token = await getToken();
     if (!token) {
       throw new Error("Failed to get token");
