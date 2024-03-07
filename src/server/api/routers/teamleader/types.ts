25f37e0016b295703e4e95613dd9907f12fde8d4
Jitse Laceur Belon <jitse.laceurbelon@student.hogent.be>
2024-03-07 13:22:48 +0100

    [feat] Teamleader data is opgehaald en omgezet naar de dataobjecten die we nodig hebben.

--- src/server/api/routers/teamleader/types.ts
+++ src/server/api/routers/teamleader/types.ts
@@ -59,68 +59,72 @@ export interface Deal {
 };
 
 export interface User {
-    id: string;
-    account: {
-        type: string;
+    data: {
         id: string;
-    };
-    first_name: string;
-    last_name: string;
-    email: string;
-    language: string;
-    telephones: string[];
-    function: string | null;
-    status: string;
-    avatar_url: string | null;
-    teams: {
-        type: string;
-        id: string;
-    }[] | null;
+        account: {
+            type: string;
+            id: string;
+        };
+        first_name: string;
+        last_name: string;
+        email: string;
+        language: string;
+        telephones: string[];
+        function: string | null;
+        status: string;
+        avatar_url: string | null;
+        teams: {
+            type: string;
+            id: string;
+        }[] | null; 
+    }
 };
 
 export interface Company {
-    id: string;
-    name: string;
-    status: string;
-    business_type: {
-        type: string;
+    data: {
         id: string;
-    } | null;
-    vat_number: string | null;
-    national_identification_number: string | null;
-    emails: {
-        type: string;
-        email: string;
-    }[] | null;
-    telephones: {
-        type: string;
-        number: string;
-    }[] | null;
-    website: string | null;
-    iban: string | null;
-    bic: string | null;
-    activity_code: string | null;
-    language: string;
-    payment_term: string | null;
-    preferred_currency: string | null;
-    invoicing_preferences: {
-        electronic_invoicing_address: string | null;
-    } | null;
-    added_at: string;
-    updated_at: string;
-    web_url: string;
-    primary_address: {
-        line_1: string | null;
-        postal_code: string | null;
-        city: string | null;
-        country: string;
-        area_level_two: string | null;
-    } | null;
-    responsible_user: {
-        type: string;
-        id: string;
-    } | null;
-    tags: string[];
+        name: string;
+        status: string;
+        business_type: {
+            type: string;
+            id: string;
+        } | null;
+        vat_number: string | null;
+        national_identification_number: string | null;
+        emails: {
+            type: string;
+            email: string;
+        }[] | null;
+        telephones: {
+            type: string;
+            number: string;
+        }[] | null;
+        website: string | null;
+        iban: string | null;
+        bic: string | null;
+        activity_code: string | null;
+        language: string;
+        payment_term: string | null;
+        preferred_currency: string | null;
+        invoicing_preferences: {
+            electronic_invoicing_address: string | null;
+        } | null;
+        added_at: string;
+        updated_at: string;
+        web_url: string;
+        primary_address: {
+            line_1: string | null;
+            postal_code: string | null;
+            city: string | null;
+            country: string;
+            area_level_two: string | null;
+        } | null;
+        responsible_user: {
+            type: string;
+            id: string;
+        } | null;
+        tags: string[];
+    }
 };
 
 export interface SimplifiedDeal {
