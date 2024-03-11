7b4a063687839d9c0637d2ecdcdfa0ceacc3ebdb
rubenschrurs <ruben@schrurs.be>
2024-03-11 16:26:22 +0100

    [feat] added columns for each deal_status
    Going forward use 'tv:...' to apply css changes to the size of the tv when sizing something that has a unique px size.

--- src/components/ui/column.tsx
+++ src/components/ui/column.tsx
@@ -3,12 +3,14 @@ import { Card, CardHeader, CardContent, CardTitle } from "./card";
 export const Column = ({
   title,
   children,
+  size,
 }: {
   title: string;
   children: React.ReactNode;
+  size?: "columnDeals" | "columnMogelijkheden";
 }) => {
   return (
-    <Card variant={"column"} size={"column"}>
+    <Card variant="column" size={size ?? "column"}>
       <CardHeader className="pb-1.5">
         <CardTitle>{title}</CardTitle>
       </CardHeader>
