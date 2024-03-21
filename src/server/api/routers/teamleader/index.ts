6ceee199f0e388d30e44c71e1634c18f1ce6e054
rubenschrurs <ruben@schrurs.be>
2024-03-21 11:53:41 +0100

    issue with jwt

--- src/server/api/routers/teamleader/index.ts
+++ src/server/api/routers/teamleader/index.ts
@@ -1,42 +1,49 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { env } from "~/env";
-import z from "zod";
-import { getDeals, refreshAccessToken, simplifyDeals } from "./utils";
-import type {SimplifiedDealArray} from "./types";
+import { getDeals, simplifyDeals } from "./utils";
+import type { SimplifiedDealArray } from "./types";
+import { authOptions, getServerAuthSession, getAuthToken } from "~/server/auth"; // Import the function to get the server auth session
 
 export const teamleaderRouter = createTRPCRouter({
   getRedirectionURL: protectedProcedure.query(() => {
     return `https://focus.teamleader.eu/oauth2/authorize?client_id=${env.TEAMLEADER_CLIENT_ID}&response_type=code&redirect_uri=${env.TEAMLEADER_REDIRECT_URL}`; // Replace with your desired URL
   }),
 
-  getDealsData : protectedProcedure.input(z.string()).query(async (accessToken) => {
+  getDealsData: protectedProcedure.query(async (options) => {
+    const { input, ctx } = options;
+    // Retrieve the token from
+    console.log("HELLO I AM HERE");
+    const token = getAuthToken();
+    console.log("Token:", token);
+    // Use the access token from the session to make API calls
+    const accessToken = "token.";
     try {
       if (!accessToken) {
         throw new Error("Access token not found");
       }
-      const deals = await getDeals(accessToken.input);
+      const deals = await getDeals(accessToken);
       if (!deals) {
         throw new Error("Failed to fetch data from Teamleader");
       }
 
       const simpleData: SimplifiedDealArray = await simplifyDeals(deals);
-      //console.log(simpleData);
       return simpleData;
     } catch (error) {
-      console.error('Error in getDealsData:',error);
+      console.error("Error in getDealsData:", error);
     }
   }),
 
-  refreshAccessToken: protectedProcedure.input(z.string()).query(async (refreshToken) => {
-    try {
-      if (!refreshToken) {
-      throw new Error("refresh token not found");
-      }
-      const tokens = await refreshAccessToken(refreshToken.input);
-      return tokens;
-    } catch (error) {
-      console.error('Error in getDeals:',error);
-    }
-  }),
-
+  // refreshAccessToken: protectedProcedure
+  //   .input(z.string())
+  //   .query(async (refreshToken) => {
+  //     try {
+  //       if (!refreshToken) {
+  //         throw new Error("refresh token not found");
+  //       }
+  //       const tokens = await refreshAccessToken(refreshToken.input);
+  //       return tokens;
+  //     } catch (error) {
+  //       console.error("Error in getDeals:", error);
+  //     }
+  //   }),
 });
