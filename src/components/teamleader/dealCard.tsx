5413712f8dfae3e613617118d84bbb2e2be4ce25
Jitse Laceur Belon <jitse.laceurbelon@student.hogent.be>
2024-04-09 13:54:53 +0200

    [feat] fixed the deals css

--- src/components/teamleader/dealCard.tsx
+++ src/components/teamleader/dealCard.tsx
@@ -3,8 +3,8 @@ import { Card, CardHeader, CardContent, CardTitle } from "../ui/card";
 import Image from "next/image";
 import { DropContext } from "~/contexts/dndProvider";
 import { useContext } from "react";
-import { url } from "inspector";
 import CompanyLogo from "./companyLogo";
+import afkortingen from "~/lib/Afkortingen.json";
 
 export default function DealCard({ deal }: { deal: SimplifiedDeal }) {
   const { activeDealId } = useContext(DropContext);
@@ -17,7 +17,7 @@ export default function DealCard({ deal }: { deal: SimplifiedDeal }) {
   return (
     <Card variant={variant} size={"deal"}>
       <CardHeader>
-        <CardTitle className="text-sm font-medium flex max-w-32 items-center">
+        <CardTitle className="text-sm font-medium flex max-w-40 items-center">
           {deal.company.logo_url ? (
             // hier checken we of de url via de google api komt
             deal.company.logo_url.startsWith(
@@ -50,8 +50,8 @@ export default function DealCard({ deal }: { deal: SimplifiedDeal }) {
         </CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col gap-1 items-end">
-        <div className="text-end text-xs bg-js_dev_role px-2 py-0.5 rounded-[14px] tv:rounded-[28px] w-fit">
-          {trimMogelijkhedenTitle(deal.title)}
+        <div className="text-end text-xs bg-js_dev_role px-2 py-0.5 rounded-[14px] tv:rounded-[28px] w-fit truncate...">
+          {trimRole(deal.title)}
         </div>
         <div className="flex gap-3 justify-end font-normal text-sm items-end">
           {deal.estimated_closing_date ? (
@@ -83,14 +83,38 @@ export default function DealCard({ deal }: { deal: SimplifiedDeal }) {
   );
 }
 
-const trimMogelijkhedenTitle = (title: string) => {
-  return title.split("(")[0];
+const trimDealTitle = (title: string) => {
+  let trimmedTitle = title.split("[")[0]?.split("(")[0];
+  trimmedTitle = replaceWords(trimmedTitle);
+  return trimmedTitle;
 };
 
-const trimDealTitle = (title: string) => {
-  const splitTitle = title.split(" ");
-  if (splitTitle.length > 2) {
-    return splitTitle.slice(0, 2).join(" ") + "...";
-  }
-  return title;
+const trimRole = (role: string) => {
+  let newRole = role.split("[")[0]?.split("(")[0];
+  newRole = newRole?.replace(/\b[A-Z]*[0-9]+[A-Z]+|[A-Z]+[0-9]+[A-Z]*\b/g, "");
+  newRole = replaceWords(newRole);
+  return newRole;
+};
+
+const replaceWords = (str: string | undefined): string => {
+  if (!str) return "";
+  const words: Record<string, string> = afkortingen.reduce(
+    (acc: Record<string, string>, { woord, afkorting }) => {
+      acc[woord] = afkorting;
+      return acc;
+    },
+    {},
+  );
+
+  const replacedStr: string = str
+    .split(/[\s/]+/)
+    .map((word: string) => {
+      if (words[word]) {
+        return words[word];
+      }
+      return word;
+    })
+    .join(" ");
+
+  return replacedStr;
 };
