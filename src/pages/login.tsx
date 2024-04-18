d02445f8702f40a12bc1687b5eb3ff3dcfc21c15
rubenschrurs <ruben@schrurs.be>
2024-04-18 14:45:34 +0200

    [refactor] swap image to have better contrast

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import Image from "next/legacy/image";
-import bannerImage from "public/images/Euricom Edited Low Res.jpg";
+import bannerImage from "public/images/Euricom-banner2.jpeg";
 import logo from "public/images/Euricom Logo.png";
 import { signIn } from "next-auth/react";
 import { Button } from "~/components/ui/button";
@@ -14,7 +14,11 @@ export default function Login() {
           objectFit="cover"
           layout="fill"
           priority={true}
+          className="absolute"
         />
+        <span className="absolute left-1/2 top-1/2 w-2/3 -translate-x-1/2 -translate-y-1/2 text-center text-3xl text-white bg-accent-foreground p-3">
+          Sales Dashboard
+        </span>
       </div>
       <div className="flex flex-1 flex-col items-center justify-center gap-4 bg-white">
         <Image
