a3df7e9962052216c9ec6f9fb26f91954eda801f
Yorne Euricom <yorne.thys@euri.com>
2024-06-12 11:16:09 +0200

    [fix] clean up sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,8 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: 'https://8914e05c96606cf841e5a97f2959b15a@o4505997166247936.ingest.us.sentry.io/4507412341981184',
-  // enabled: process.env.NODE_ENV === 'production',
-  enabled: true,
+  enabled: process.env.NODE_ENV === 'production',
   environment: process.env.VERCEL_ENV ?? 'local',
   integrations: [
     Sentry.feedbackIntegration({
